fd48433838a6a2bcdc350a87d34164b9af289ebf
sunil <sunilraghu199@gmail.com>
2023-04-24 11:38:17 +0530

    Added Merge Sort

--- SortingAlgorithm.cs
+++ SortingAlgorithm.cs
@@ -12,9 +12,69 @@ namespace basic
             int[] arr1 = { 64, 34, 25, 12, 22, 11, 90 };
             BubbleSort(arr1);
             QuickSort(arr1, 0, arr1.Length - 1);
-            printArray(arr1);
+            MergeSort(arr1, 0, arr1.Length - 1);
             InsertionSort(arr1);
             SelectionSort(arr);
+            printArray(arr1);
+        }
+
+
+        static void MergeSort(int[] arr, int left, int right)
+        {
+            if (left < right)
+            {
+                int mid = (left + right) / 2;
+                MergeSort(arr, left, mid);
+                MergeSort(arr, mid + 1, right);
+                Merge(arr, left, mid, right);
+
+            }
+        }
+        static void Merge(int[] arr, int left, int mid, int right)
+        {
+            int[] temp = new int[right - left + 1];
+            int i = left;
+            int j = mid + 1;
+
+            int k = 0;
+
+            while (i <= mid && j <= right)
+            {
+                if (arr[i] < arr[j])
+                {
+                    temp[k] = arr[i];
+                    i++;
+                }
+                if (arr[j] < arr[i])
+                {
+                    temp[k] = arr[j];
+                    j++;
+                }
+                k++;
+            }
+
+            while (i <= mid)
+            {
+                temp[k] = arr[i];
+                i++;
+                k++;
+            }
+
+            while (j <= right)
+            {
+                temp[k] = arr[j];
+                j++;
+                k++;
+            }
+
+
+
+            for (i = left, k = 0; i <= right; i++, k++)
+            {
+                arr[i] = temp[k];
+
+            }
+
         }
 
         private static void QuickSort(int[] arr1, int left, int right)
